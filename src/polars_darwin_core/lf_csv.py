26ab17784c3aabb0f0dbe23a4680bbf90d53491d
Corey Farwell <coreyf@rwell.org>
2025-09-05 19:29:27 -0400

    simplfy schema reading

--- src/polars_darwin_core/lf_csv.py
+++ src/polars_darwin_core/lf_csv.py
@@ -21,83 +21,24 @@ class DarwinCoreLazyFrame:
     linters and type-checkers can understand.
     """
 
-    # Common required fields in Darwin Core datasets
-    EXPECTED_SCHEMA: Dict[str, Type[pl.DataType] | pl.DataType] = {
-        # Required core fields
-        "scientificName": pl.Utf8,
+    # Columns that need a type override.
+    # Polars can infer most types, but dates, datetimes, and booleans stored as
+    # strings need to be specified.
+    SCHEMA_OVERRIDES: Dict[str, Type[pl.DataType] | pl.DataType] = {
         "kingdom": kingdom_data_type,
-        # Optional but common fields
-        "phylum": pl.Utf8,
-        "class": pl.Utf8,
-        "order": pl.Utf8,
-        "family": pl.Utf8,
-        "genus": pl.Utf8,
-        "species": pl.Utf8,
-        # Geolocation fields
-        "decimalLatitude": pl.Float64,
-        "decimalLongitude": pl.Float64,
-        "continent": pl.Utf8,
-        "country": pl.Utf8,
-        "countryCode": pl.Utf8,
-        "stateProvince": pl.Utf8,
-        "county": pl.Utf8,
-        "municipality": pl.Utf8,
-        "locality": pl.Utf8,
-        "verbatimLocality": pl.Utf8,
-        "minimumElevationInMeters": pl.Float64,
-        "maximumElevationInMeters": pl.Float64,
-        "verbatimElevation": pl.Utf8,
-        "minimumDepthInMeters": pl.Float64,
-        "maximumDepthInMeters": pl.Float64,
-        "verbatimDepth": pl.Utf8,
-        "geodeticDatum": pl.Utf8,
-        "coordinateUncertaintyInMeters": pl.Float64,
-        "georeferenceProtocol": pl.Utf8,
-        "georeferenceSources": pl.Utf8,
-        "georeferenceVerificationStatus": pl.Utf8,
-        # Occurrence fields
-        "basisOfRecord": pl.Utf8,
-        "occurrenceID": pl.Utf8,
-        "eventDate": pl.Utf8,
-        "catalogNumber": pl.Utf8,
-        "recordNumber": pl.Utf8,
-        "recordedBy": pl.Utf8,
-        "individualCount": pl.Int64,
-        "sex": pl.Utf8,
-        "lifeStage": pl.Utf8,
-        "reproductiveCondition": pl.Utf8,
-        "occurrenceStatus": pl.Utf8,
-        "occurrenceRemarks": pl.Utf8,
-        # Record-level fields
-        "type": pl.Utf8,
-        "modified": pl.Utf8,
-        "language": pl.Utf8,
-        "license": pl.Utf8,
-        "rightsHolder": pl.Utf8,
-        "accessRights": pl.Utf8,
-        "bibliographicCitation": pl.Utf8,
-        "references": pl.Utf8,
-        "institutionID": pl.Utf8,
-        "collectionID": pl.Utf8,
-        "datasetID": pl.Utf8,
-        "institutionCode": pl.Utf8,
-        "collectionCode": pl.Utf8,
-        "datasetName": pl.Utf8,
-        "ownerInstitutionCode": pl.Utf8,
-        "informationWithheld": pl.Utf8,
-        "dataGeneralizations": pl.Utf8,
-        "dynamicProperties": pl.Utf8,
+        # Dates and datetimes
+        # "modified": pl.Datetime,
+        # "eventDate": pl.Datetime,
+        # "dateIdentified": pl.Date,
+        # "georeferencedDate": pl.Date,
+        # "lastInterpreted": pl.Datetime,
+        # "lastParsed": pl.Datetime,
+        # "lastCrawled": pl.Datetime,
+        # Booleans
+        "hasCoordinate": pl.Boolean,
+        "hasGeospatialIssues": pl.Boolean,
+        "repatriated": pl.Boolean,
     }
-
-    # SCHEMA_OVERRIDES = {
-    #     "decimalLatitude": pl.Float64(),
-    #     "decimalLongitude": pl.Float64(),
-    #     "taxonKey": pl.UInt64(),
-    #     "verbatimScientificName": pl.String(),
-    #     "order": pl.String(),
-    #     "recordedBy": pl.String(),
-    #     "kingdom": kingdom_enum,
-    # }
 
     def __init__(self, inner: pl.LazyFrame):
         """Initialize the Darwin Core LazyFrame wrapper.
@@ -128,7 +69,7 @@ class DarwinCoreLazyFrame:
 
         inner = pl.scan_csv(
             path,
-            schema_overrides=DarwinCoreLazyFrame.EXPECTED_SCHEMA,
+            schema_overrides=DarwinCoreLazyFrame.SCHEMA_OVERRIDES,
             quote_char=None,
             separator="\t",
         )
@@ -262,11 +203,18 @@ class DarwinCoreLazyFrame:
         core_file_rel, has_header, separator, columns = cls._parse_meta(meta_path)
         data_path = base_dir / core_file_rel
 
+        schema_from_meta = {
+            col: cls.SCHEMA_OVERRIDES[col]
+            for col in columns
+            if col in cls.SCHEMA_OVERRIDES
+        }
+        scan_csv_kwargs.setdefault("schema_overrides", {}).update(schema_from_meta)
+
         inner = pl.scan_csv(
             data_path,
             separator=separator,
             has_header=has_header,
-            new_columns=columns if has_header is False else None,
+            new_columns=columns if not has_header else None,
             **scan_csv_kwargs,
         )
 
