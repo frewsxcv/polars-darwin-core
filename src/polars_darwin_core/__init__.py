67a7fa9d3359a7b77df6485f7487cc7e17af85af
Corey Farwell <coreyf@rwell.org>
2025-09-05 23:21:10 -0400

    export other files

--- src/polars_darwin_core/__init__.py
+++ src/polars_darwin_core/__init__.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 from polars_darwin_core.dataframe import DarwinCoreDataFrame
 from polars_darwin_core.lazyframe import DarwinCoreLazyFrame
+from polars_darwin_core.darwin_core import Kingdom, TAXONOMIC_RANKS
 
 __all__ = [
     "DarwinCoreDataFrame",
     "DarwinCoreLazyFrame",
+    "Kingdom",
+    "TAXONOMIC_RANKS",
 ]
--- src/polars_darwin_core/archive.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-"""Helpers for reading unpacked Darwin Core Archives (DwC-A)."""
